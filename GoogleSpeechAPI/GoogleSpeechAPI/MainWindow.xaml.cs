a6d4843d283e540c9c8f8f3e6da3a1093ae9b173
Kurt Nistelberger <kurt.nistelberger@hotmail.com>
2016-05-05 18:15:17 +0200

    Clean UP Code

--- GoogleSpeechAPI/GoogleSpeechAPI/MainWindow.xaml.cs
+++ GoogleSpeechAPI/GoogleSpeechAPI/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ using NAudio.Wave;
 using CUETools.Codecs;
 using CUETools.Codecs.FLAKE;
 
+using System.Web.Script.Serialization;
 
 namespace GoogleSpeechAPI
 {
@@ -30,109 +31,117 @@ namespace GoogleSpeechAPI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public class Person
+        {
+            public string transcript { get; set; }
+            public float confidence { get; set; }
+        }
 
+        public class Results
+        {
+            public string result { get; set; }
+            public float confidence { get; set; }
+        }
+
+        private const string folder_name = "SpeechFiles";
         private MMDevice device;
-        private IWaveIn waveIn;
+        private IWaveIn waveIn = null;
         private WaveFileWriter writer;
         private string outputFilename;
+        private string outputFilenameFlac;
         private string outputFolder;
-
-        private string filename;
-
+       
         public MainWindow()
         {
             InitializeComponent();
-            listAllInputDevices();
+            initInputDevices();
+            initWaveInDevice();
         }
+
+        #region ButtonEvents
 
         private void btn_startRecording_Click(object sender, RoutedEventArgs e)
         {
             startRecordingClick();
         }
 
-        private void convertToFlac()// Stream sourceStream, Stream destinationStream)
+        private void btn_stopRecording_Click(object sender, RoutedEventArgs e)
         {
-            // wav-File Stream
-            var sourceStream = new FileStream(System.IO.Path.Combine(outputFolder, outputFilename), FileMode.Open);
-            //var destinationStream = new FileStream(System.IO.Path.Combine(outputFolder, "myFlacTest.flac"), FileMode.Open);
+            StopRecording();
+        }
 
-
-            var audioSource = new WAVReader(null, sourceStream);
-            try
-            {
-              /*  if (audioSource.PCM.SampleRate != 16000)
-                {
-                    throw new InvalidOperationException("Incorrect frequency - WAV file must be at 16 KHz.");
-                }*/
-
-                var buff = new AudioBuffer(audioSource, 0x10000);
-                // var flakeWriter = new FlakeWriter(null, destinationStream, audioSource.PCM);
-                var flakeWriter = new FlakeWriter(System.IO.Path.Combine(outputFolder, "myFlacTest.flac"), audioSource.PCM);
-
-                flakeWriter.CompressionLevel = 8;
-                while (audioSource.Read(buff, -1) != 0)
-                {
-                    flakeWriter.Write(buff);
-                }
-                flakeWriter.Close();
-                //destinationStream.Close();
-            }
-            finally
-            {
-                audioSource.Close();
-            }
-        }    
-
-
-    private void listAllInputDevices()
+        private void btn_sendFlacFile_Click(object sender, RoutedEventArgs e)
         {
+            sendFlacFile();
+        }
+
+        #endregion
+
+        private void initInputDevices()
+        {     
             var deviceEnum = new MMDeviceEnumerator();
             var devices = deviceEnum.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active).ToList();
-
+            
             foreach (var dev in devices)
             {
-                Console.WriteLine(dev.DeviceFriendlyName);
+                cb_mics.Items.Add(dev);
                 dev.AudioEndpointVolume.Mute = false; // turn on microphone
-                device = dev; // I have only on microphone device
             }
+
+            if (devices.Count > 0)
+            {
+                cb_mics.SelectedIndex = 0; // default select first microphone
+                device = (MMDevice)cb_mics.SelectedItem;
+            }  
         }
 
         private void startRecordingClick()
         {
-            outputFolder = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "NAudioDemo");
+            if (writer != null)
+            {
+                writer.Dispose();
+                writer = null;
+            }
+
+            if(waveIn != null)
+            {
+                waveIn.Dispose();
+                waveIn = null;
+                initWaveInDevice();
+            }
+
+            // save recorded file in tmpFolder
+            outputFolder = System.IO.Path.Combine(System.IO.Path.GetTempPath(), folder_name);
             Directory.CreateDirectory(outputFolder);
             Console.WriteLine("Path: " + outputFolder);
 
-            waveIn = CreateWaveInDevice();
-            outputFilename = String.Format("NAudioDemo {0:yyy-MM-dd HH-mm-ss}.wav", DateTime.Now);
+            outputFilenameFlac = String.Format("capture_{0:yyy-MM-dd HH-mm-ss}", DateTime.Now);
+            outputFilename = outputFilenameFlac + ".wav";
+            outputFilenameFlac += ".flac";
+
             writer = new WaveFileWriter(System.IO.Path.Combine(outputFolder, outputFilename), waveIn.WaveFormat);
             waveIn.StartRecording();
         }
 
-        private IWaveIn CreateWaveInDevice()
+        private void initWaveInDevice()
         {
             // create new WaveIn Device to capture microphone input
-
-            IWaveIn newWaveIn;
-
-            // use WaveIn, mybe try WaceInEvent
-            newWaveIn = new WaveIn();
-            newWaveIn.WaveFormat = new WaveFormat(16000, 1);//(8000, 1);
-
-            newWaveIn.DataAvailable += OnDataAvailable;
-            newWaveIn.RecordingStopped += OnRecordingStopped;
-            return newWaveIn;
+            waveIn = new WaveIn();
+            waveIn.WaveFormat = new WaveFormat(16000, 1);
+      
+            waveIn.DataAvailable += OnDataAvailable;
+            waveIn.RecordingStopped += OnRecordingStopped;
         }
 
         void OnDataAvailable(object sender, WaveInEventArgs e)
-        {
-            Console.WriteLine("Data Available");
+        {      
             if (writer == null)
                 return;
-
+            
             writer.Write(e.Buffer, 0, e.BytesRecorded);
+
             int secondsRecorded = (int)(writer.Length / writer.WaveFormat.AverageBytesPerSecond);
-            if (secondsRecorded >= 3)
+            if (secondsRecorded >= 5)
             {
                 StopRecording();
             }
@@ -140,6 +149,8 @@ namespace GoogleSpeechAPI
 
         void OnRecordingStopped(object sender, StoppedEventArgs e)
         {
+            if (writer == null)
+                return;
             writer.Dispose();
             writer = null;
         }
@@ -147,19 +158,47 @@ namespace GoogleSpeechAPI
         // when we stop recording, over an event or something else
         void StopRecording()
         {
-            if (writer == null)
-                return;
 
-            Console.WriteLine("StopRecording");
-            waveIn.StopRecording();
-            writer.Dispose();
-            writer = null;
-            convertToFlac();
+            if (writer != null)
+            {
+                writer.Dispose();
+                writer = null;
+            }
+
+
+            while (!File.Exists(System.IO.Path.Combine(outputFolder, outputFilenameFlac)))
+            {
+                convertToFlac();
+                Console.WriteLine("convert file");
+            }
+            
+            sendFlacFile();
+        }
+
+        private void convertToFlac()
+        {
+            // wav-File Stream
+            FileStream sourceStream = new FileStream(System.IO.Path.Combine(outputFolder, outputFilename), FileMode.Open);
+            WAVReader audioSource = new WAVReader(System.IO.Path.Combine(outputFolder, outputFilename), sourceStream);
+
+
+            AudioBuffer buff = new AudioBuffer(audioSource, 0x10000);
+            FlakeWriter flakeWriter = new FlakeWriter(System.IO.Path.Combine(outputFolder, outputFilenameFlac), audioSource.PCM);
+
+            flakeWriter.CompressionLevel = 8;
+            while (audioSource.Read(buff, -1) != 0)
+            {
+                flakeWriter.Write(buff);
+            }
+
+            sourceStream.Close();
+            flakeWriter.Close();
+            audioSource.Close();
         }
 
         void sendFlacFile()
         {
-            FileStream fileStream = File.OpenRead("C:\\Users\\niskurt-win10\\AppData\\Local\\Temp\\NAudioDemo\\myFlacTest.flac");
+            FileStream fileStream = File.OpenRead(System.IO.Path.Combine(outputFolder, outputFilenameFlac));
             MemoryStream memoryStream = new MemoryStream();
             memoryStream.SetLength(fileStream.Length);
             fileStream.Read(memoryStream.GetBuffer(), 0, (int)fileStream.Length);
@@ -187,12 +226,59 @@ namespace GoogleSpeechAPI
 
                 txt_result.Text = result;
                 Console.WriteLine(result);
-            }
-        }
 
-        private void btn_sendFlacFile_Click(object sender, RoutedEventArgs e)
-        {
-            sendFlacFile();
+                //var serializer = new JavaScriptSerializer();
+                //var deserializedResult = serializer.Deserialize<List<Person>>(result);
+
+                if (result.StartsWith("{\"result\":[]}\n{\"result\":[{\"alternative\":"))
+                {
+                    result = result.Substring("{\"result\":[]}\n{\"result\":[{\"alternative\":".Length);
+                }
+
+                if (result.EndsWith(",\"final\":true}],\"result_index\":0}\n"))
+                {
+                    result = result.Substring(0, result.Length - ",\"final\":true}],\"result_index\":0}\n".Length);
+                }
+
+
+                var serializer = new JavaScriptSerializer();
+                var deserializedResult = serializer.Deserialize<List<Person>>(result);
+
+                txt_result.Text = deserializedResult[0].transcript;
+
+                Console.WriteLine("testte");
+
+                /*
+
+            "{\"result\":[]}\n
+            {\"result\":[{\"alternative\":[{\"transcript\":\"hello Mark how are you\",\"confidence\":0.96041793},{\"transcript\":\"hello Mark are you\"},{\"transcript\":\"hello Mark how are youuu\"},{\"transcript\":\"hello Mark how are ya\"},{\"transcript\":\"hello Mark how are young\"}],\"final\":true}],\"result_index\":0}\n"
+
+
+
+            [
+                {"transcript":"hello Mark how are you","confidence":0.98267901},
+                {"transcript":"hello Mark how are U"},
+                {"transcript":"hello Mark how are youuu"},
+                {"transcript":"hello Mark how are you I"},
+                {"transcript":"hello Mark how are your"}
+            ]
+
+            {\"result\":[]}{\"result\":[{\"alternative\":
+
+                    {"result":[]}
+                    {"result":[
+                        {"alternative":[
+                            {"transcript":"hello Mark how are you","confidence":0.88823956},
+                            {"transcript":"hello Mark"},
+                            {"transcript":"hello Mark I"},
+                            {"transcript":"hello Mark I am"},
+                            {"transcript":"hello Mark Ohio"}],
+                        "final":true}],
+                    "result_index":0}
+
+                 */
+
+            }
         }
     }
 }
